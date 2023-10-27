1a29a63365e1f119ac044a5994e3567258902198
Stjepan Grgic <grgic.stjepan@gmail.com>
2023-10-27 17:03:42 +0100

    Fixed env key

--- src/lib/segment.ts
+++ src/lib/segment.ts
@@ -2,11 +2,6 @@ import "client-only"
 
 import { AnalyticsBrowser } from "@segment/analytics-next"
 
-const key =
-  process.env.NODE_ENV === "production"
-    ? process.env.NEXT_PUBLIC_SEGMENT_WRITE_KEY
-    : ""
-
 export const analytics = AnalyticsBrowser.load({
-  writeKey: key as string,
+  writeKey: process.env.NEXT_PUBLIC_SEGMENT_WRITE_KEY as string,
 })
