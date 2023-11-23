0bad19210508fe5c2e4fbbb63bfd67e6559da914
Stjepan Grgic <grgic.stjepan@gmail.com>
2023-11-23 13:20:53 +0000

    SliceSimulator page shows up localy

---
+++ src/app/slice-simulator/layout.tsx
@@ -0,0 +1,10 @@
+const SliceSimulatorLayout = ({ children }: { children: React.ReactNode }) => {
+  return (
+    // has to be en-GB as that is the default locale
+    <html lang="en-GB">
+      <body>{children}</body>
+    </html>
+  )
+}
+
+export default SliceSimulatorLayout
