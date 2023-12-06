006a933cbc439c04edf870ada8770e922e50cebf
Stjepan Grgic <grgic.stjepan@gmail.com>
2023-12-06 15:49:57 +0000

    Cleanup and Mobile CSS started on the previous commit

--- src/components/PageLayout.tsx
+++ src/components/PageLayout.tsx
@@ -8,18 +8,12 @@ type PageLayoutParams = {
   locale: string
 }
 
-// export default async function NavbarData({ locale }: { locale: string }) {
-//   const client = createClient()
-//   const links = await client.getSingle("navbar", { lang: locale })
-// }
-
 export default async function PageLayout({
   locale,
   children,
 }: PageLayoutParams) {
   const client = createClient()
   const NavbarData = await client.getSingle("navbar", { lang: locale })
-
   const FooterData = await client.getSingle("footer")
   const FooterLanguages = await sortLocales(
     (await client.getRepository()).languages,
