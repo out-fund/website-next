83185a1da6d3800154e7eb7adc48c43b171978b9
Stjepan Grgic <grgic.stjepan@gmail.com>
2024-01-17 13:24:46 +0000

    Build fix

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,10 +1,10 @@
-import type { Metadata } from "next"
-import { createClient } from "@/prismicio"
+// import type { Metadata } from "next"
+// import { createClient } from "@/prismicio"
 
-type PageProps = {
-  params: { uid: string; locale: string }
-  searchParams?: { [key: string]: string | string[] | undefined }
-}
+// type PageProps = {
+//   params: { uid: string; locale: string }
+//   searchParams?: { [key: string]: string | string[] | undefined }
+// }
 
 export default function GlobalLayout({
   children,
@@ -14,23 +14,23 @@ export default function GlobalLayout({
   return <>{children}</>
 }
 
-export async function generateMetadata({
-  params,
-}: PageProps): Promise<Metadata> {
-  const client = createClient()
-  const globalSEO = await client.getSingle("global_seo", {
-    lang: params.locale,
-  })
-  return {
-    metadataBase: new URL("https://out.fund"),
-    title: {
-      template: "%s | Outfund",
-      default: `${globalSEO.data.site_title}`,
-    },
-    description: globalSEO.data.meta_description || "",
-    referrer: "origin-when-cross-origin",
-    openGraph: {
-      images: [globalSEO.data.og_image.url || ""],
-    },
-  }
-}
+// export async function generateMetadata({
+//   params,
+// }: PageProps): Promise<Metadata> {
+//   const client = createClient()
+//   const globalSEO = await client.getSingle("global_seo", {
+//     lang: params.locale,
+//   })
+//   return {
+//     metadataBase: new URL("https://out.fund"),
+//     title: {
+//       template: "%s | Outfund",
+//       default: `${globalSEO.data.site_title}`,
+//     },
+//     description: globalSEO.data.meta_description || "",
+//     referrer: "origin-when-cross-origin",
+//     openGraph: {
+//       images: [globalSEO.data.og_image.url || ""],
+//     },
+//   }
+// }
