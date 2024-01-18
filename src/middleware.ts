14d605749d03d103b5d9944a55c3734a21acfbff
Stjepan Grgic <grgic.stjepan@gmail.com>
2024-01-18 19:34:25 +0000

    Added documents

--- src/middleware.ts
+++ src/middleware.ts
@@ -46,7 +46,7 @@ export async function middleware(request: NextRequest) {
 export const config = {
   // Do not localize these paths
   matcher: [
-    "/((?!api|_next/static|_next/image|slice-simulator|sitemap.xml|images|country-selector|robots.txt|favicon.ico).*)",
+    "/((?!api|_next/static|_next/image|slice-simulator|sitemap.xml|images|docs|misc|country-selector|robots.txt|favicon.ico).*)",
   ],
 }
 
