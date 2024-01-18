ea59eac03583efd5f2e71d23cb6f8db819eac999
Stjepan Grgic <grgic.stjepan@gmail.com>
2024-01-18 21:34:34 +0000

    Added error pages

--- src/app/[locale]/error.tsx
+++ src/app/[locale]/error.tsx
@@ -1,6 +1,20 @@
+"use client" // Error components must be Client Components
+
+import { useEffect } from "react"
 import Link from "next/link"
 
-export default function NotFound() {
+export default function Error({
+  error,
+  reset,
+}: {
+  error: Error & { digest?: string }
+  reset: () => void
+}) {
+  useEffect(() => {
+    // Log the error to an error reporting service
+    // console.error(error)
+  }, [error])
+
   return (
     <div className="w-full h-full flex flex-col items-center mt-5 gap-2">
       <h1 className="font-bold">Page Not Found</h1>
