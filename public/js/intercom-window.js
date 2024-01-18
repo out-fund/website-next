67640e33736f590980e98190364b0fb76b27cd23
Stjepan Grgic <grgic.stjepan@gmail.com>
2024-01-18 18:00:23 +0000

    Public JS fixes

---
+++ public/js/intercom-window.js
@@ -0,0 +1,4 @@
+window.intercomSettings = {
+  api_base: "https://api-iam.intercom.io",
+  app_id: "myeba9bl"
+};
--- src/app/[locale]/layout.tsx
+++ src/app/[locale]/layout.tsx
@@ -33,8 +33,8 @@ export default function RootLayout({
         ></script> */}
       </body>
       <GoogleAnalytics gaId="G-V6R3GNYBT3" />
-
       <Script src="/js/segment.js" />
+      <Script src="/js/intercom-window.js" />
       <Script src="/js/intercom.js" />
     </html>
   )
