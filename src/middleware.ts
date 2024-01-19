6a74360811c184883d622e843b4827bf474ecb32
Stjepan Grgic <grgic.stjepan@gmail.com>
2024-01-19 20:29:27 +0000

    Fixed country redirects

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,11 +1,46 @@
 import { createLocaleRedirect } from "@prismicio/next"
 import { createClient } from "@/prismicio"
-import { NextRequest } from "next/server"
+import { NextResponse, type NextRequest } from "next/server"
 
 export async function middleware(request: NextRequest) {
+  if (request.nextUrl.pathname.includes("/us/")) {
+    return NextResponse.redirect(
+      new URL(request.url.replace(`/us/`, "/en-us/")),
+    )
+  }
+  if (request.nextUrl.pathname.includes("/au/")) {
+    return NextResponse.redirect(
+      new URL(request.url.replace(`/au/`, "/en-au/")),
+    )
+  }
+  if (request.nextUrl.pathname.includes("/nl/")) {
+    return NextResponse.redirect(
+      new URL(request.url.replace(`/nl/`, "/nl-nl/")),
+    )
+  }
+  if (request.nextUrl.pathname.includes("/es/")) {
+    return NextResponse.redirect(
+      new URL(request.url.replace(`/es/`, "/es-es/")),
+    )
+  }
+  if (request.nextUrl.pathname.includes("/de/")) {
+    return NextResponse.redirect(
+      new URL(request.url.replace(`/de/`, "/de-de/")),
+    )
+  }
+  if (request.nextUrl.pathname.includes("/uk/")) {
+    return NextResponse.redirect(
+      new URL(request.url.replace(`/uk/`, "/en-gb/")),
+    )
+  }
+  if (request.nextUrl.pathname.includes("/ie/")) {
+    return NextResponse.redirect(
+      new URL(request.url.replace(`/ie/`, "/en-ie/")),
+    )
+  }
+
   const client = createClient()
   const redirect = await createLocaleRedirect({ client, request })
-
   if (redirect) {
     return redirect
   }
