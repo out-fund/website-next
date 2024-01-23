828355400de8d25269b15a838f447a3efeaed5a4
Stjepan Grgic <grgic.stjepan@gmail.com>
2024-01-23 14:50:02 +0000

    Reverted middleware

--- src/middleware.ts
+++ src/middleware.ts
@@ -10,26 +10,40 @@ export async function middleware(request: NextRequest) {
     )
   }
   // Country redirects
-  if (request.nextUrl.pathname.includes("/us")) {
-    return NextResponse.redirect(new URL(request.url.replace(`/us`, "/en-us/")))
+  if (request.nextUrl.pathname.includes("/us/")) {
+    return NextResponse.redirect(
+      new URL(request.url.replace(`/us/`, "/en-us/")),
+    )
   }
-  if (request.nextUrl.pathname.includes("/au")) {
-    return NextResponse.redirect(new URL(request.url.replace(`/au`, "/en-au/")))
+  if (request.nextUrl.pathname.includes("/au/")) {
+    return NextResponse.redirect(
+      new URL(request.url.replace(`/au/`, "/en-au/")),
+    )
   }
-  if (request.nextUrl.pathname.includes("/nl")) {
-    return NextResponse.redirect(new URL(request.url.replace(`/nl`, "/nl-nl/")))
+  if (request.nextUrl.pathname.includes("/nl/")) {
+    return NextResponse.redirect(
+      new URL(request.url.replace(`/nl/`, "/nl-nl/")),
+    )
   }
-  if (request.nextUrl.pathname.includes("/es")) {
-    return NextResponse.redirect(new URL(request.url.replace(`/es`, "/es-es/")))
+  if (request.nextUrl.pathname.includes("/es/")) {
+    return NextResponse.redirect(
+      new URL(request.url.replace(`/es/`, "/es-es/")),
+    )
   }
-  if (request.nextUrl.pathname.includes("/de")) {
-    return NextResponse.redirect(new URL(request.url.replace(`/de`, "/de-de/")))
+  if (request.nextUrl.pathname.includes("/de/")) {
+    return NextResponse.redirect(
+      new URL(request.url.replace(`/de/`, "/de-de/")),
+    )
   }
-  if (request.nextUrl.pathname.includes("/uk")) {
-    return NextResponse.redirect(new URL(request.url.replace(`/uk`, "/en-gb/")))
+  if (request.nextUrl.pathname.includes("/uk/")) {
+    return NextResponse.redirect(
+      new URL(request.url.replace(`/uk/`, "/en-gb/")),
+    )
   }
-  if (request.nextUrl.pathname.includes("/ie")) {
-    return NextResponse.redirect(new URL(request.url.replace(`/ie`, "/en-ie/")))
+  if (request.nextUrl.pathname.includes("/ie/")) {
+    return NextResponse.redirect(
+      new URL(request.url.replace(`/ie/`, "/en-ie/")),
+    )
   }
 
   const client = createClient()
