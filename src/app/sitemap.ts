d98ede7fb97536c5952b03a4df8f5d9dcd863bef
Stjepan Grgic <grgic.stjepan@gmail.com>
2024-02-01 10:56:09 +0100

    Changed date format on sitemap.xml

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -1,6 +1,7 @@
 import { MetadataRoute } from "next"
 import { createClient } from "@/prismicio"
 import * as prismic from "@prismicio/client"
+import moment from "moment"
 
 export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
   const client = createClient()
@@ -20,21 +21,21 @@ export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
   // TODO: change weekly to monthly for pages that are not updated often later
   const homePagesURLs = homePages.map((page) => ({
     url: `https://out.fund/${page.lang}/`,
-    lastModified: page.last_publication_date,
+    lastModified: moment(page.last_publication_date).format("YYYY-MM-DD"),
     changeFrequency: "weekly" as const,
     priority: 1,
   }))
 
   const pageURLs = pages.map((page) => ({
     url: `https://out.fund/${page.lang}/${page.uid}/`,
-    lastModified: page.last_publication_date,
+    lastModified: moment(page.last_publication_date).format("YYYY-MM-DD"),
     changeFrequency: "weekly" as const,
     priority: 0.9,
   }))
 
   const blogPostsURLs = blogPosts.map((page) => ({
     url: `https://out.fund/${page.lang}/blog/${page.uid}/`,
-    lastModified: page.last_publication_date,
+    lastModified: moment(page.last_publication_date).format("YYYY-MM-DD"),
     changeFrequency: "weekly" as const,
     priority: 0.5,
   }))
