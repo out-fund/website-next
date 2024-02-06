e2da21187ab94b3644b3bed523744b85dc584b1e
Stjepan Grgic <steven.grgic@out.fund>
2024-02-06 17:24:32 +0000

    Added bing ads tag

--- src/app/[lang]/layout.tsx
+++ src/app/[lang]/layout.tsx
@@ -21,9 +21,13 @@ export default function RootLayout({
     <html lang={langToHtmlLang(params.lang)} className={cn(inter.className)}>
       <body className="box-border antialiased">{children}</body>
       <GoogleAnalytics gaId="G-V6R3GNYBT3" />
+      <Script strategy="afterInteractive" id="bing-adds">
+        {`(function(w,d,t,r,u){var f,n,i;w[u]=w[u]||[],f=function(){var o={ti:"148001090"};o.q=w[u],w[u]=new UET(o),w[u].push("pageLoad")},n=d.createElement(t),n.src=r,n.async=1,n.onload=n.onreadystatechange=function(){var s=this.readyState;s&&s!=="loaded"&&s!=="complete"||(f(),n.onload=n.onreadystatechange=null)},i=d.getElementsByTagName(t)[0],i.parentNode.insertBefore(n,i)})(window,document,"script","//bat.bing.com/bat.js","uetq");`}
+      </Script>
     </html>
   )
 }
+
 export const metadata: Metadata = {
   metadataBase: new URL("https://out.fund"),
   icons: {
