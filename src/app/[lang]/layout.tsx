2ca90e137657e24ded980a728e5fbb6db5838946
Steven G <steven.grgic@out.fund>
2024-03-20 11:59:06 +0000

    Added preview component

--- src/app/[lang]/layout.tsx
+++ src/app/[lang]/layout.tsx
@@ -1,11 +1,14 @@
+// import { createClient } from "@/prismicio"
+import { PrismicPreview } from "@prismicio/next"
+import { repositoryName } from "@/prismicio"
+
 import Script from "next/script"
 import { GoogleAnalytics } from "@next/third-parties/google"
 import type { Metadata } from "next"
-import { createClient } from "@/prismicio"
 
 import { inter, dmSans } from "@/lib/font"
 import { langToHtmlLang } from "@/lib/utils"
-import { cn } from "@/lib/utils"
+// import { cn } from "@/lib/utils"
 import "@/styles/globals.css"
 
 export default function RootLayout({
@@ -24,6 +27,7 @@ export default function RootLayout({
       <Script strategy="afterInteractive" id="microsoftAds-UET">
         {`(function(w,d,t,r,u){var f,n,i;w[u]=w[u]||[],f=function(){var o={ti:"148001090"};o.q=w[u],w[u]=new UET(o),w[u].push("pageLoad")},n=d.createElement(t),n.src=r,n.async=1,n.onload=n.onreadystatechange=function(){var s=this.readyState;s&&s!=="loaded"&&s!=="complete"||(f(),n.onload=n.onreadystatechange=null)},i=d.getElementsByTagName(t)[0],i.parentNode.insertBefore(n,i)})(window,document,"script","//bat.bing.com/bat.js","uetq");`}
       </Script>
+      <PrismicPreview repositoryName={repositoryName} />
     </html>
   )
 }
